7688d5adb25a0eea06b0509a1529d9bda398e562
Bayonet <124799042+Bayosoft@users.noreply.github.com>
2023-03-11 22:47:07 +0100

    Added Unity project with NoesisGUI

---
+++ Exploration-Roguelike/Assets/GUI/Combat/CombatView.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_5_3_OR_NEWER
+#define NOESIS
+using Noesis;
+#else
+using System;
+using System.Windows;
+using System.Windows.Controls;
+#endif
+
+namespace Exploration_Roguelike.GUI.Combat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class CombatView : UserControl
+    {
+        public CombatView()
+        {
+            this.InitializeComponent();
+        }
+
+#if NOESIS
+        void InitializeComponent()
+        {
+            NoesisUnity.LoadComponent(this);
+        }
+#endif
+    }
+}
---
+++ Exploration-Roguelike/Blend/App.xaml.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Windows;
+
+namespace Exploration_Roguelike
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
