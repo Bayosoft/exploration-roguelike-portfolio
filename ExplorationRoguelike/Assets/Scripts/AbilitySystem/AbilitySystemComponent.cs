1e263128748bc960362cf9da78e61c00bdc071ef
Zeetrax <Zeetrex70@gmail.com>
2023-06-25 14:21:21 +0200

    Updated AbilitySystem folder structure
    Folder structure of AbilitySystem scripts moved together in Scripts\AbilitySystem\

--- ExplorationRoguelike/Assets/Scripts/AbilitySystem/AbilitySystemComponent.cs
+++ ExplorationRoguelike/Assets/Scripts/AbilitySystem/AbilitySystemComponent.cs
@@ -16,7 +16,7 @@ namespace ExplorationRoguelike
 
         public GameplayTagContainer OwnedGameplayTags { get; private set; }
 
-        public void OnEnable()
+        void Start()
         {
             ActiveGameplayEffects = new(this);
         }
---
+++ ExplorationRoguelike/Assets/Scripts/AbilitySystem/GameplayAttributeSet.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ExplorationRoguelike
+{
+    public class GameplayAttributeSet : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+
+    }
+}
