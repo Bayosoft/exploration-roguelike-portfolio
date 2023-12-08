8de8d0bbd1a4b07f68759dba40d9fb99570dfceb
Bayonet <124799042+Bayosoft@users.noreply.github.com>
2023-12-08 23:19:12 +0100

    Merged combatviewmodel into combatstate to simplify code

--- ExplorationRoguelike/Assets/Scripts/Combat/CombatStateComponent.cs
+++ ExplorationRoguelike/Assets/Scripts/Combat/CombatStateComponent.cs
@@ -1,9 +1,16 @@
 using ExplorationRoguelike.AbilitySystem;
+using ExplorationRoguelike.AbilitySystem.Abilities;
 using ExplorationRoguelike.Characters;
 using ExplorationRoguelike.Characters.NonPlayerCharacters;
 using ExplorationRoguelike.Characters.PlayerCharacter;
 using ExplorationRoguelike.Combat.Events;
+using ExplorationRoguelike.GUI.Card;
+using ExplorationRoguelike.GUI.Character;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
 namespace ExplorationRoguelike.Combat
@@ -31,16 +38,38 @@ namespace ExplorationRoguelike.Combat
         public Player player;
         public NpcTurnComponent enemyTurnComponent;
         public PlayerTurnComponent playerTurnComponent;
-        
+      
+        public GameObject cardPrefab;
+        public GameObject healthPrefab;
+        public TextMeshProUGUI manaText;
+        public ObservableCollection<GameObject> HealthViews { get; set; }
+        public ObservableCollection<GameObject> CardViews { get; set; }
+
+        // public ObservableCollection<AbilitySO> DrawnCards { get { return new ObservableCollection<AbilitySO>(_combat.CardDeckComponent.CardsDrawn); } }
+
+        public object SelectedCard { get; set; }
+        public TextMeshProUGUI enemyIntentText;
+
         [SerializeField]
         private ScriptableEvent combatEvent;
-
 
         public void Awake()
         {
             enemies = new List<CombatNpc>();
+            CardViews = new ObservableCollection<GameObject>();
+            HealthViews = new ObservableCollection<GameObject>();
         }
+        // Start is called before the first frame update
+        void Start()
+        {
+            playerTurnComponent.CardDeckComponent.CardsDrawn.CollectionChanged += UpdateCards;
+            playerTurnComponent.CardDeckComponent.OnManaChanged += UpdateMana;
+            enemyTurnComponent.NpcCombatComponent.OnDeclaredIntent += UpdateEnemyIntent;
 
+            SpawnHealthViews();
+            StartCombat();
+
+        }
         public void Initialize(GameObject playerPrefab, GameObject enemyPrefab)
         {
             var playerInstance = Instantiate(playerPrefab);
@@ -52,11 +81,7 @@ namespace ExplorationRoguelike.Combat
             playerTurnComponent = (PlayerTurnComponent)player.TurnComponent;
             enemyTurnComponent = (NpcTurnComponent)enemies[0].TurnComponent;
         }
-        // Start is called before the first frame update
-        void Start()
-        {
-            StartCombat();
-        }
+
 
         public void StartCombat()
         {
@@ -83,6 +108,93 @@ namespace ExplorationRoguelike.Combat
             }
         }
 
+        private void UpdateMana(object sender, int newMana)
+        {
+            manaText.text = $"Mana: {newMana}/4";
+        }
+
+        private void UpdateEnemyIntent(object sender, GameplayAbility intent)
+        {
+            enemyIntentText.text = $"Enemy Intent: {intent.generalTags.First().ToString()}";
+        }
+        private void SpawnHealthViews()
+        {
+            List<ICombatant> combatants = new(enemies);
+
+            // Player
+            var healthView = Instantiate(healthPrefab, this.transform);
+
+            healthView.GetComponent<HealthViewModel>().Initialize(player.HealthComponent);
+            healthView.transform.localPosition = new Vector2(-400, 0);
+            healthView.transform.localScale = Vector2.one;
+            HealthViews.Add(healthView);
+
+            // Enemies
+            foreach (var combatant in combatants)
+            {
+                GameObject eHealthView = Instantiate(healthPrefab, this.transform);
+                eHealthView.GetComponent<HealthViewModel>().Initialize(combatant.HealthComponent);
+                eHealthView.transform.localPosition = new Vector2(400, 0);
+                eHealthView.transform.localScale = Vector2.one;
+                HealthViews.Add(eHealthView);
+            }
+        }
+
+        public void UpdateCards(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            //different kind of changes that may have occurred in collection
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                CardView c = (CardView)e.NewItems[0];
+                GameObject cardView = c.gameObject;
+                cardView.transform.parent = gameObject.transform;
+                cardView.transform.localPosition = new Vector2(CardViews.Count * 100, 0);
+                cardView.transform.localPosition = new Vector2(-300 + (CardViews.Count * 100), -350f);
+                cardView.transform.localScale = Vector2.one;
+                cardView.transform.SetAsLastSibling();
+
+                cardView.SetActive(true);
+                CardViews.Add(cardView);
+            }
+            if (e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                foreach (CardView removedCard in e.OldItems)
+                {
+                    foreach (GameObject cardView in CardViews.ToList())
+                    {
+                        if (cardView.GetComponent<CardView>() == removedCard)
+                        {
+                            cardView.SetActive(false);
+                            CardViews.Remove(cardView);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void OnTryPlayCard(ConcreteEventArgs args)
+        {
+            var eventArgs = args.ValidateEventArgs<TryPlayCardEventArgs>();
+            if (eventArgs.CardView != null && playerTurnComponent.MyTurn)
+            {
+                playerTurnComponent.CardDeckComponent.PlayCard(eventArgs.CardView, new List<AbilitySystemComponent>() { enemies[0].AbilitySystemComponent });
+            }
+        }
+
+        public void OnEndTurn()
+        {
+            if (playerTurnComponent.MyTurn)
+            {
+                playerTurnComponent.EndTurn();
+            }
+        }
+
+        public void OnCombatEvent(ConcreteEventArgs eventArgs)
+        {
+            var combatEventArgs = eventArgs.ValidateEventArgs<CombatEventArgs>(this);
+
+        }
         public void OnDeath<T>(T deadCombatant)
         {
             if (deadCombatant is Player)
