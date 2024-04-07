dfcb5526af895f1a7ce5e817f83f98085e2fcb14
Bayosoft <124799042+Bayosoft@users.noreply.github.com>
2024-04-08 00:42:28 +0200

    WIP implement flex grid layout

---
+++ ExplorationRoguelike/Assets/FlexibleGridLayout.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ExplorationRoguelike
+{
+    public class FlexibleGridLayout : LayoutGroup
+    {
+     // TODO:   https://youtu.be/CGsEJToeXmA?t=398
+        public override void CalculateLayoutInputVertical()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void SetLayoutHorizontal()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void SetLayoutVertical()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+    }    
+}
